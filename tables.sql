198ecf1f3fc4766480164efbfc43703a50b3308f
Sudeepthi57 <sandepogusudeepthi@gmail.com>
2025-11-06 15:58:17 +0530

    second commit

--- tables.sql
+++ tables.sql
@@ -1,16 +1,10 @@
 
 
 
-SELECT COUNT(*) FROM Customers;
-SELECT COUNT(*) FROM Orders;
 
-SELECT * FROM Customers LIMIT 5;
-SELECT * FROM Orders LIMIT 5;
 
-SELECT Customer_ID, COUNT(*) AS Orders_before
-FROM Orders
-WHERE Order_Date BETWEEN 'SELECT Customer_ID, COUNT(*) AS Orders_Before
-FROM Orders
-WHERE Order_Date BETWEEN '2024-01-01' AND '2024-12-31'
-GROUP BY Customer_ID;2024-01-01' AND '2024-12-31'
-GROUP BY Customer_ID;
+SELECT customer_id, signup_date, city, acquisition_channel
+FROM customers
+WHERE DATE(substr(signup_date, 7, 4) || '-' || substr(signup_date, 4, 2) || '-' || substr(signup_date, 1, 2))
+      BETWEEN '2024-01-01' AND '2024-12-31';
+
