0a27fa5af1303d21beb06f7de91294839d0b3416
Sudeepthi57 <sandepogusudeepthi@gmail.com>
2025-11-08 15:28:10 +0530

    fourth commit

--- tables.sql
+++ tables.sql
@@ -1,8 +1,11 @@
 
 
 
+SELECT COUNT(*) FROM Customers;
+SELECT COUNT(*) FROM Orders;
 
-
+SELECT * FROM Customers LIMIT 5;
+SELECT * FROM Orders LIMIT 5;
 SELECT customer_id, signup_date, city, acquisition_channel
 FROM customers
 WHERE DATE(substr(signup_date, 7, 4) || '-' || substr(signup_date, 4, 2) || '-' || substr(signup_date, 1, 2))
